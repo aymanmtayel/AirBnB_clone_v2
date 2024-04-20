831556d094a7aa936fa2d036a720112d90913bdc
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-20 21:19:15 +0200

    Task01

--- models/base_model.py
+++ models/base_model.py
@@ -2,29 +2,28 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         storage.new(self)
         if len(kwargs) > 0:
                 for key, value in kwargs.items():
-                       if key == 'created_at' or key == 'updated_at':
-                              value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                              if key != '__class__':
-                                     setattr(self, key, value)
-                                     self.__dict__.update(kwargs)
+                        if key == 'created_at' or key == 'updated_at':
+                                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        if key != '__class__':
+                               setattr(self, key, value)
+                               self.__dict__.update(kwargs)
                                      
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
