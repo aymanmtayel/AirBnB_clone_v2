9cf090ab315b04a4cf59ae84d0b8cfbf36b6777f
aymanmtayel <eng.aymantayel@gmail.com>
2024-05-04 01:51:24 +0300

    add super user

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 #Bash script that sets up your web servers for the deployment of web_static.
 sudo apt -y update
 sudo apt -y install nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "Hello from the test web page" > /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo echo "Hello from the test web page" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a \
 \
     location /hbnb_static/ {\
     	alias /data/web_static/current/;\
     }' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
