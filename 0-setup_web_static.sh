d0f3317dda7d5268d58eaad361bdd81df405ad1d
aymanmtayel <eng.aymantayel@gmail.com>
2024-05-04 03:00:31 +0300

    Task 0 own

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Bash script that sets up your web servers for the deployment of web_static.
+
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Hello from the test web page" | sudo tee /data/web_static/releases/test/index.html
